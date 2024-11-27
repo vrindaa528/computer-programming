226e30ef8825045a17edc5d6da5b347c2d357cb6
vrindaa528 <v1486132@gmail.com>
2024-11-27 20:39:29 +0530

    string_q9.py

---
+++ coding-assignment/string/string_q9.py
@@ -0,0 +1,8 @@
+#Capitalize Each Word
+#Question: Write a program to capitalize the first letter of each word in a string.
+
+#Solution:
+
+s = "hello world"
+capitalized_s = s.title()
+print(capitalized_s)  #"Hello World"
