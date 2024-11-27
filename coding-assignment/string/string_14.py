323754a38aac4a671b6cb54362722b81371d20aa
vrindaa528 <v1486132@gmail.com>
2024-11-27 20:46:48 +0530

    string_14.py

---
+++ coding-assignment/string/string_14.py
@@ -0,0 +1,9 @@
+#Count Substring Occurrences
+#Question: Write a program to count the number of times a substring occurs in a string.
+
+#Solution:
+
+s = "hello world, hello universe"
+substring = "hello"
+count = s.count(substring)
+print(count)  # Output: 2
