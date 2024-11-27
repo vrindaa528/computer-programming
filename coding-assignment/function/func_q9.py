ffbaa164896c57e0491e3c10e825cd192150e125
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:35:04 +0530

    func_q9.py

---
+++ coding-assignment/function/func_q9.py
@@ -0,0 +1,11 @@
+#Find Maximum
+#Question: Write a function to find the maximum of three numbers.
+
+#Solution:
+
+def find_max(a, b, c):
+    return max(a, b, c)
+
+# Example
+result = find_max(3, 7, 5)
+print(result)  # Output: 7
