32d7af234070c7d93b4fce13a0b3768efedfbf03
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:43:12 +0530

    func_q13.py

---
+++ coding-assignment/function/string_q13.py
@@ -0,0 +1,12 @@
+#Find Length of String
+Q#uestion: Write a function to find the length of a string.
+
+Solution:
+
+
+def string_length(s):
+    return len(s)
+
+# Example
+result = string_length("hello")
+print(result)  #output :5
