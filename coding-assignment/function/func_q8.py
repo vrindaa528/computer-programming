59eb36cac136e1ed8402f6d45186a6fa924a4ded
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:33:59 +0530

    func_q8.py

---
+++ coding-assignment/function/func_q8.py
@@ -0,0 +1,11 @@
+#Reverse a String
+#Question: Write a function to reverse a string.
+
+#Solution:
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example
+result = reverse_string("hello")
+print(result)  # Output: "olleh"
