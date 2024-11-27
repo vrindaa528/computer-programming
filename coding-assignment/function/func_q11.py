ad33cb73c316f5f901b79b2578ee904fa3c3b1d6
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:36:55 +0530

    func_q11.py

---
+++ coding-assignment/function/func_q11.py
@@ -0,0 +1,11 @@
+#Calculate Power
+#Question: Write a function to calculate the power of a number.
+
+#Solution:
+
+def power(base, exp):
+    return base ** exp
+
+# Example
+result = power(2, 3)
+print(result)  # Output: 8
