ed9e54e32303131a2ccff3956a4a4170a0540167
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:55:16 +0530

    tuple_6.py

---
+++ coding-assignment/tuple/tuple_6.py
@@ -0,0 +1,8 @@
+#Filter Tuples by Condition
+#Question: Write a program to filter tuples where the sum of elements is greater than a specified value.
+
+#Solution:
+tuples_list = [(1, 2), (3, 4), (0, 0), (10, -5)]
+threshold = 3
+filtered_tuples = [t for t in tuples_list if sum(t) > threshold]
+print(filtered_tuples)  # Output: [(3, 4)]
