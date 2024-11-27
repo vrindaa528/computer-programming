762913fe8d065b4c46704b342c519796751d8a46
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:05:02 +0530

    list_q5.py

---
+++ coding-assignment/list/list_q5.py
@@ -0,0 +1,8 @@
+#List to String
+#Question: Write a program to convert a list of characters into a string.
+
+#Solution:
+
+char_list = ['a', 'b', 'c', 'd']
+string = ''.join(char_list)
+print(string)  # Output: abcd
