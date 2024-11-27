93f8f32b26ef2ecc54fb0bd86380261793172e38
vrindaa528 <v1486132@gmail.com>
2024-11-27 20:37:47 +0530

    string_q8.py

---
+++ coding-assignment/string/string_q8.py
@@ -0,0 +1,8 @@
+#Replace Substring
+#Question: Write a program to replace a substring within a string.
+
+#Solution:
+
+s = "hello world"
+new_s = s.replace("world", "Python")
+print(new_s)  # Output: "hello Python"
