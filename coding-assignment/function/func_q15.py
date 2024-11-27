0688e40303de270895f2aa1dc6357e5c6c8dded0
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:46:08 +0530

    func_q15.py

---
+++ coding-assignment/function/func_q15.py
@@ -0,0 +1,12 @@
+#Convert Celsius to Fahrenheit
+#Question: Write a function to convert Celsius to Fahrenheit.
+
+Solution:
+
+python
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9/5 + 32
+
+# Example
+result = celsius_to_fahrenheit(25)
+print(result)  # Output: 77.0
