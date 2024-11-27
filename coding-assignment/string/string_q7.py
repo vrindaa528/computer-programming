64aa6e2fb704d661bd2c8fb9d6e61cff5525e653
vrindaa528 <v1486132@gmail.com>
2024-11-27 20:28:50 +0530

    string_q7.py

---
+++ coding-assignment/string/string_q7.py
@@ -0,0 +1,8 @@
+#Convert to Title Case
+#Question: Write a program to convert a string to title case.
+
+#Solution:
+
+s = "hello world"
+title_case = s.title()
+print(title_case)  # Output: "Hello World"
