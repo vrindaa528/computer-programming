698dc4b61951ee27834c9192cec7e94f78ee6671
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:31:27 +0530

    list_q10.py

---
+++ coding-assignment/list/list_q10.py
@@ -0,0 +1,8 @@
+#Reverse a List
+#Question: Write a program to reverse a list.
+
+#Solution:
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list)  # Output: [5, 4, 3, 2, 1]
