37729f47d3d1768c28914d47a55e2a3329273085
vrindaa528 <v1486132@gmail.com>
2024-11-27 22:23:20 +0530

    func_q2.py

---
+++ coding-assignment/function/func_q2.py
@@ -0,0 +1,11 @@
+#Check Even or Odd
+#Question: Write a function to check if a number is even or odd.
+
+#Solution:
+
+def is_even(n):
+    return n % 2 == 0
+
+# Example
+result = is_even(4)
+print(result)  # Output: True
