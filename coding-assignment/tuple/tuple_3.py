1301678e124ab94ff4cb4ca0ab6a434edf8d1e93
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:51:07 +0530

    tuple_3.py

---
+++ coding-assignment/tuple/tuple_3.py
@@ -0,0 +1,8 @@
+#Sort a Tuple
+#Question: Write a program to sort a tuple of integers.
+
+#Solution:
+
+my_tuple = (3, 1, 4, 1, 5, 9)
+sorted_tuple = tuple(sorted(my_tuple))
+print(sorted_tuple)  # Output: (1, 1, 3, 4, 5, 9)
