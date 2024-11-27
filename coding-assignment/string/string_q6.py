246eba07618e2182090e67beea7b6cb7cb4c5ab6
vrindaa528 <v1486132@gmail.com>
2024-11-27 20:27:57 +0530

    string_q6.py

---
+++ coding-assignment/string/string_q6.py
@@ -0,0 +1,9 @@
+#Find Substring
+#Question: Write a program to find a substring within a string.
+
+#Solution:
+
+s = "hello world"
+substring = "world"
+index = s.find(substring)
+print(index)  # Output: 6
