7aa50ef5ba432e791597f4c071a9faed6233e093
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:11:37 +0530

    list_q7.py

---
+++ coding-assignment/list/list_q7.py
@@ -0,0 +1,10 @@
+#Insert an Element
+#Question: Write a program to insert an element at a specified position in a list.
+
+#Solution:
+
+my_list = [1, 2, 3, 4, 5]
+element = 99
+position = 2
+my_list.insert(position, element)
+print(my_list)  # Output: [1, 2, 99, 3, 4, 5]
