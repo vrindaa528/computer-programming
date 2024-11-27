d6af9222757abf6d88f456338f9b9266454078f3
vrindaa528 <v1486132@gmail.com>
2024-11-27 19:51:52 +0530

    tuple_4.py

---
+++ coding-assignment/tuple/tuple_4.py
@@ -0,0 +1,8 @@
+#Slice a Tuple
+#Question: Write a program to slice a tuple from the 2nd to the 4th element.
+
+#Solution:
+
+my_tuple = (1, 2, 3, 4, 5, 6)
+sliced_tuple = my_tuple[1:4]
+print(sliced_tuple)  # Output: (2, 3, 4)
